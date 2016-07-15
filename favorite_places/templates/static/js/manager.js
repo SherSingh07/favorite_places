8bffcaaff512b0d241ad3b2d34cd43d772637efe
SherSingh <wbsher91@gmail.com>
2016-07-15 23:06:29 +0530

    adding angular js module (draft)

---
+++ favorite_places/templates/static/js/manager.js
@@ -0,0 +1,23 @@
+/*
+------------------------------------------------------------------------------
+				PLACES MANAGER ANGULAR JS SCRIPT
+------------------------------------------------------------------------------
+*/
+
+// creating the module
+var myApp = 
+angular
+.module("placeModule", [])
+.controller("placeController", function($scope) {	
+
+	$scope.message = "Places Manager Demo By Singh";
+
+	// some place objects
+	$scope.places = [ ];
+
+
+	// function to add new place
+	$scope.addTask = function() {
+		//$scope.places.push({ title: $scope.new_place, completed: false});
+	}
+});
