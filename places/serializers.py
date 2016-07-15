8fb28b578b417255b2b0a445da853ba4f244fc06
SherSingh <wbsher91@gmail.com>
2016-07-16 01:25:06 +0530

    adding place serializer

--- places/serializers.py
+++ places/serializers.py
@@ -1,9 +1,13 @@
 from django.contrib.auth.models import User
 
 from rest_framework import serializers
-from places.models import Category
+from places.models import Category, Place
 
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
+
+class PlaceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Place
