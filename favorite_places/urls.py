01aeabcd260e1f2ec8536d7421ad30dd3dfff5a2
SherSingh <wbsher91@gmail.com>
2016-07-15 22:24:20 +0530

    adding home page view

--- favorite_places/urls.py
+++ favorite_places/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+import places.views as places_views
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', places_views.home, name='home'),
 ]
