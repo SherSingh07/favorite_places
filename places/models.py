b7943ea741026eaef398fc7299cf942f6e866901
SherSingh <wbsher91@gmail.com>
2016-07-15 21:06:17 +0530

    adding category model for a place

--- places/models.py
+++ places/models.py
@@ -2,4 +2,24 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    """
+    model for category for a place
+
+    we will be storing places in category,
+    eg. travel, shopping, work, party etc.
+    """
+    user = models.ForeignKey(User)	
+    title = models.CharField(max_length=128, verbose_name=_('Title'))
+    description = models.CharField(max_length=1024, verbose_name=_('Description'))
+
+    # preview/thumbnail image for an place, that use probably will take with camera
+    img = models.FileField(upload_to = 'photos/')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name=_('Created at'))
+
+    def __unicode__(self):
+        """
+        function returns unicode representation of category
+        """
+        return "%s" % self.title
