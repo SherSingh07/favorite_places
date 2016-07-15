bdf93d1373a074da288dd8eaf5baf3a3adbf70cf
SherSingh <wbsher91@gmail.com>
2016-07-16 01:01:42 +0530

    get all categories on start

--- favorite_places/templates/static/js/manager.js
+++ favorite_places/templates/static/js/manager.js
@@ -8,18 +8,28 @@
 var myApp = 
 angular
 .module("placeModule", [])
-.controller("placeController", function($scope) {	
+.controller("placeController", function($scope, $http) {	
 
 	$scope.message = "Places Manager Demo By Singh";
 
 	// some place objects
 	$scope.places = [ ];
+	$scope.categories = [{title: 'aaa'}];
 
-
+        $scope.GetAllCategories = function () {
+             $http.get("http://localhost:8000/categories/")
+                 .then(function(response){ 
+                     $scope.categories = response.data; 
+                 });
+        };
+            
 	// function to add new place
 	$scope.addTask = function() {
 		//$scope.places.push({ title: $scope.new_place, completed: false});
 	}
+
+        // show categories on start
+        $scope.GetAllCategories();
 });
 
 myApp.config(function($interpolateProvider) {
