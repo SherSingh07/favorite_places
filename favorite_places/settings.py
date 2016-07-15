f8a0a257e717167dae64e3259f8d0c3620b40d37
SherSingh <wbsher91@gmail.com>
2016-07-15 22:38:03 +0530

    template paths

--- favorite_places/settings.py
+++ favorite_places/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'favorite_places.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['favorite_places/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
