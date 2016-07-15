c09e62740330a066839ff424d337fcddf0a3bb9d
SherSingh <wbsher91@gmail.com>
2016-07-15 22:19:26 +0530

    adding imports

--- places/models.py
+++ places/models.py
@@ -1,6 +1,8 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils.translation import ugettext_lazy as _
 
 
 class Category(models.Model):
